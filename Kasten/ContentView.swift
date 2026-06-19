f85c1c64aab21f87629eb09970dadacf754bb1d5
inugamine <zh20d0m0iztxkg5f5914@gmail.com>
2026-06-20 02:23:27 +0900

    Initial Commit

---
+++ Kasten/ContentView.swift
@@ -0,0 +1,25 @@
+//
+// ContentView.swift
+// Kasten
+//
+// Created by inugaminé on 2026/06/20.
+//
+  
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Kasten/KastenApp.swift
@@ -0,0 +1,18 @@
+//
+// KastenApp.swift
+// Kasten
+//
+// Created by inugaminé on 2026/06/20.
+//
+  
+
+import SwiftUI
+
+@main
+struct KastenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
